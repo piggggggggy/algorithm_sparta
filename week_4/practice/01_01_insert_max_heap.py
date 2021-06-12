2d29b0d5c5fc2e70469304ba3335c5b857a81861
piggggggggy <pyt4105@gmail.com>
2021-06-13 03:16:24 +0900

    알고리즘강의_4주차 힙까지

---
+++ week_4/practice/01_01_insert_max_heap.py
@@ -0,0 +1,21 @@
+class MaxHeap:
+    def __init__(self):
+        self.items = [None]
+
+    def insert(self, value):
+        # 구현해보세요!
+        self.items.append(value)
+        i = len(self.items)-1
+        while i > 1 :
+            if self.items[i] > self.items[i//2]:
+                self.items[i],self.items[i//2] = self.items[i//2],self.items[i]
+                i = i //2
+            else:
+                break
+
+max_heap = MaxHeap()
+max_heap.insert(3)
+max_heap.insert(4)
+max_heap.insert(2)
+max_heap.insert(9)
+print(max_heap.items)  # [None, 9, 4, 2, 3] 가 출력되어야 합니다!
