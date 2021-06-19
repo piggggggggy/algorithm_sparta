83ba685a7ae55b592cf25f7f9af9c69924082c3d
piggggggggy <pyt4105@gmail.com>
2021-06-19 20:18:04 +0900

    알고리즘마라톤_백트래킹 공부

--- algorithm_marathon/1~60/35_15650.py
+++ algorithm_marathon/1~60/35_15650.py
@@ -4,9 +4,28 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
-lst = [i+1 for i in range(N)]
+visited = [False] * N
+out = []
 
-dict = {}
-for i in lst:
-    dict[i] = lst
+def backtracking(depth, n, m):
+    if depth == m:
+        print(*out)
+        return
 
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            out.append(i+1)
+            backtracking(depth+1, n, m)
+            out.pop()
+ 
+            for j in range(i+1, n):
+                visited[j] = False
+
+backtracking(0, N, M) # N = 4, M = 2
+# 1 2
+# 1 3
+# 1 4
+# 2 3
+# 2 4
+# 3 4
---
+++ study/15649_backtracking.py
@@ -0,0 +1,23 @@
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+visited = [False] * N
+out = []
+
+def backtracking(depth, n, m):
+    if depth == m:  # 탈출 조건
+        # print(' '.join(map(str, out))) # list 를 str로 합쳐 출력
+        print(*out)
+        return
+
+    for i in range(N): # check 하면서 탐사
+        if visited[i]: # visited가 False이면, 즉, 탐사 안했다면
+            continue
+        visited[i] = True # 탐사 내용 얻데이트
+        out.append(i+1) # 탐사 내용 (출력용)
+        backtracking(depth+1, n, m) # 다음 깊이 탐색
+        visited[i] = False # 탐사완료 후 빠꾸하기 위해
+        out.pop() # 재귀함수 내에서 depth == m 이라는 조건을 만족하고 출력하고 나옴
+backtracking(0, N, M)
